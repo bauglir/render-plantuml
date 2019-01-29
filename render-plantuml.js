ff44a7b2024c3595ab37d0de0413109e087da2df
Joris Kraak <me@joriskraak.nl>
2019-01-29 16:38:00 +0100

    feat: render PlantUML license using public demo server

--- render-plantuml.js
+++ render-plantuml.js
@@ -1,3 +1,17 @@
-class RenderPlantUML extends HTMLElement {}
+class RenderPlantUML extends HTMLElement {
+  constructor() {
+    super();
+
+    fetch('http://www.plantuml.com/plantuml/svg/oybCJiqhJWK0').then(response =>
+      response.text().then(svgContent => {
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+
+        shadowRoot.appendChild(
+          document.createRange().createContextualFragment(svgContent)
+        )
+      })
+    );
+  }
+}
 
 window.customElements.define('render-plantuml', RenderPlantUML);
