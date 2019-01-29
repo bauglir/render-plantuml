f96477b88b8f3fcb05242f67bb381112f7b6ef2c
Joris Kraak <me@joriskraak.nl>
2019-01-29 17:26:57 +0100

    feat: allow specification of 'render mode' (PNG or SVG)

--- render-plantuml.js
+++ render-plantuml.js
@@ -3,17 +3,49 @@ class RenderPlantUML extends HTMLElement {
     super();
 
     const publicDemoServerAddress = 'http://www.plantuml.com/plantuml';
+    const validRenderModes = [ 'png', 'svg' ];
+
     const plantUmlServerAddress = this.getAttribute('server') || publicDemoServerAddress;
+    const renderMode = (this.getAttribute('renderMode') || 'svg').toLowerCase();
 
-    fetch(`${plantUmlServerAddress}/svg/oybCJiqhJWK0`).then(response =>
-      response.text().then(svgContent => {
-        const shadowRoot = this.attachShadow({ mode: 'open' });
+    this.renderedContainer = this.attachShadow({ mode: 'open' });
 
-        shadowRoot.appendChild(
-          document.createRange().createContextualFragment(svgContent)
-        )
-      })
-    );
+    const styleSheet = document.createElement('style');
+    styleSheet.textContent = '.error { color: red }';
+    this.renderedContainer.appendChild(styleSheet);
+
+    if (validRenderModes.indexOf(renderMode) < 0) {
+      const errorMessage = document.createElement('div');
+      errorMessage.classList.add('error');
+      errorMessage.innerText = `Invalid render mode '${renderMode}'. `
+        + `Must be one of '${validRenderModes.join('\', \'')}'.`;
+
+      this.renderedContainer.appendChild(errorMessage);
+    } else {
+      fetch(`${plantUmlServerAddress}/${renderMode}/oybCJiqhJWK0`).then(response => {
+        switch (renderMode) {
+          case 'svg':
+            this.renderAsSvg(response);
+            break;
+          default:
+            this.renderAsPng(response);
+        }
+      });
+    }
+  }
+
+  renderAsPng(response) {
+    response.blob().then(pngContent => {
+      const image = document.createElement('img');
+      image.src = window.URL.createObjectURL(pngContent);
+      this.renderedContainer.appendChild(image);
+    });
+  }
+
+  renderAsSvg(response) {
+    response.text().then(svgContent => this.renderedContainer.appendChild(
+        document.createRange().createContextualFragment(svgContent)
+    ));
   }
 }
 
